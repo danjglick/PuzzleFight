d9d9c524834c46432d9838b4be04cf124b971080
danjglick <danjglick@gmail.com>
2017-11-02 15:22:03 -0400

    added devise

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise(
+    :database_authenticatable, 
+    :registerable, 
+    :recoverable, 
+    :rememberable, 
+    :trackable, 
+    :validatable
+  )
   has_many :scores
   has_many :levels, through: :scores
-  
   validates :username, presence: true
   validates :password, presence: true
 end
