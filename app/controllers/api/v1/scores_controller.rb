60288c3f38f7fc4e23dca9b64eab92234f7762c1
danjglick <danjglick@gmail.com>
2017-11-10 11:59:42 -0500

    fixed routes bug and disappearing board bug

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::ScoresController < ApplicationController
   skip_before_action :verify_authenticity_token, raise: false
-  before_action :authenticate_user!
   
   def index
     render json: {
